32615a904786259268e048afa3b70301c3fd388c
OpinAiSwift <bryam1398@gmail.com>
2020-10-11 12:54:01 -0500

    Actualización

--- src/utility.h
+++ src/utility.h
@@ -2,7 +2,7 @@
 #ifndef _UTILITY_H
 #define _UTILITY_H
 
-void Ini_FunParameter();
+void Upd_FunParameter();
 float Fun_VRefADC(); // Funcion para la lectura del voltaje de alimentacion del ADC de AVR
 double Fun_DewPoint(double Val_TempC, double Val_HumeR); // Punto de rocio
 float Fun_ConTemperature(int RawADC, float A, float B, float C);
