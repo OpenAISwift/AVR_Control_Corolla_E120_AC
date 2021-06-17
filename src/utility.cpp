a3d1354dd61269de558cf870ce3a5e62b92bada7
Swift <bryam1398@gmail.com>
2021-06-16 23:56:27 -0500

    Agregado comentarios punto de rocio

--- src/utility.cpp
+++ src/utility.cpp
@@ -22,13 +22,16 @@ float Fun_VRefADC()
 }
 double Fun_DewPoint(double Val_TempC, double Val_HumeR)
 {
+	// Presión de saturación de vapor
 	double RATIO = 373.15 / (273.15 + Val_TempC);
 	double RHS = -7.90298 * (RATIO - 1);
 	RHS += 5.02808 * log10(RATIO);
 	RHS += -1.3816e-7 * (pow(10, (11.344 * (1 - 1 / RATIO))) - 1);
 	RHS += 8.1328e-3 * (pow(10, (-3.49149 * (RATIO - 1))) - 1);
 	RHS += log10(1013.246);
+	// El factor -3 es para ajustar las unidades - Presión de vapor SVP * humedad
 	double VP = pow(10, RHS - 3) * Val_HumeR;
+	// DEWPOINT = F(Vapor Pressure)
 	double T = log(VP / 0.61078);
 	return (241.88 * T) / (17.558 - T);
 }
